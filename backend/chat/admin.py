fb8fea3051b64f0ca9efa91dbfc313e638c071e3
Woundrite <ndbendale2004@gmail.com>
2026-03-29 10:45:10 +0530

    Added chat models to admin page

--- backend/chat/admin.py
+++ backend/chat/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+import chat.models as chat_models
+
+admin.site.register(chat_models.Room)
+admin.site.register(chat_models.Message)
